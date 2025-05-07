ba17ac84bd78f8ada9dace364b4c72d08ad77d61
Dsmedinas <dsmedinas1@gmail.com>
2025-05-07 13:39:50 -0500

    Add files via upload

---
+++ Corte1/Calculator/calculadora_2.py
@@ -0,0 +1,21 @@
+#self permite acceder a los atributos y métodos de la clase
+
+class Operaciones:               #creamos una clase llamada Calculator que agrupará todas las funciones de la calculadora
+ 
+    def sumar(self, a, b):            #Recibe dos números (a y b) y devuelve la suma
+        return a+b            #suma los parámetros y devuelve el resultado
+
+    def restar(self, a, b):          #Recibe dos números (a y b) y devuelve la resta
+        return a-b            #resta los parámetros y devuelve el resultado
+
+    def multiplicar(self, a, b):           #Recibe dos números (a y b) y devuelve la multiplicación
+        return a*b            #multiplica los parámetros y devuelve el resultado
+
+    def dividir(self, a, b):         #recibe dos números (a y b) y devuelve su división
+        try:                        #con try: se intenta ejecutar a / b
+            return a/b                  #se puede usar // para que arroge el resultado entero
+        except ZeroDivisionError:       #si b es cero, Python lanza la excepción ZeroDivisionError
+            return "Error: no se puede dividir por cero"
+
+    def modulo(self, a, b):         #devuelve el residuo de la división (a % b)
+        return a % b            #aplica el módulo con los parámetros y devuelve el resultado
---
+++ Corte1/Calculator/main.py
@@ -0,0 +1,12 @@
+#Ejemplos
+from calculadora_2 import Operaciones
+
+opera = Operaciones()      
+
+# Llamar a la función
+print(opera.sumar(6, 7))
+print(opera.restar(45, 11))
+print(opera.multiplicar(3, 2))
+print(opera.dividir(10, 0))      #este ejemplo mostrará el mensaje de error con la división
+print(opera.dividir(100, 5))      
+print(opera.modulo(999,15))  
