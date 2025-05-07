9ce66a10bb11b48a1050932aed046f37929ddc36
Dsmedinas <dsmedinas1@gmail.com>
2025-05-07 13:35:26 -0500

    Add files via upload

--- Corte1/Calculator/calculadora.py
+++ Corte1/Calculator/calculadora.py
@@ -11,7 +11,7 @@ class Operaciones:               #creamos una clase llamada Calculator que agrup
     def multiplicar(self, a, b):           #Recibe dos números (a y b) y devuelve la multiplicación
         return a*b            #multiplica los parámetros y devuelve el resultado
 
-    def dividir(self, a, b):         #recibe dos números (a y b) y devuelve su suma
+    def dividir(self, a, b):         #recibe dos números (a y b) y devuelve su división
         try:                        #con try: se intenta ejecutar a / b
             return a/b                  #se puede usar // para que arroge el resultado entero
         except ZeroDivisionError:       #si b es cero, Python lanza la excepción ZeroDivisionError
@@ -19,13 +19,3 @@ class Operaciones:               #creamos una clase llamada Calculator que agrup
 
     def modulo(self, a, b):         #devuelve el residuo de la división (a % b)
         return a % b            #aplica el módulo con los parámetros y devuelve el resultado
-
-#Ejemplos
-if __name__ == "__main__":          #ejecuta este código solo si el archivo se usa directamente, no cuando otro lo importe
-    opera = Operaciones()           #crea un objeto "opera" de la clase Operaciones para usar sus funciones
-    print(opera.sumar(15, 7))
-    print(opera.restar(10, 11))
-    print(opera.multiplicar(3, 2))
-    print(opera.dividir(10, 0))      #este ejemplo mostrará el mensaje de error con la división
-    print(opera.dividir(15, 4)) 
-    print(opera.modulo(999,15))  
